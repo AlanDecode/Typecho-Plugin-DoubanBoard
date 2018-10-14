34b049aa89f96169b840ae2cc891be6eeabe9823
AlanDecode <zytbuaa1415@gmail.com>
2018-10-14 17:59:49 +0800

    调整显示效果，调整静态文件路径

--- Plugin.php
+++ Plugin.php
@@ -81,7 +81,7 @@ class DoubanBoard_Plugin implements Typecho_Plugin_Interface
      */
     public static function header()
     {
-        echo '<link rel="stylesheet" href="/usr/plugins/DoubanBoard/assets/DoubanBoard.css?v='.DoubanBoard_Plugin_VERSION.'" />';
+        echo '<link rel="stylesheet" href="/usr/plugins/DoubanBoard/assets/DoubanBoard.0.1.css?v='.DoubanBoard_Plugin_VERSION.'" />';
         if (!empty(Helper::options()->plugin('DoubanBoard')->loadJQ) && in_array('jq', Helper::options()->plugin('DoubanBoard')->loadJQ))
         {
             echo '<script src="/usr/plugins/DoubanBoard/assets/jquery.min.js"></script>';
@@ -96,7 +96,7 @@ class DoubanBoard_Plugin implements Typecho_Plugin_Interface
      */
     public static function footer()
     {
-        echo '<script type="text/javascript" src="/usr/plugins/DoubanBoard/assets/DoubanBoard.js?v='.DoubanBoard_Plugin_VERSION.'"></script>';
+        echo '<script type="text/javascript" src="/usr/plugins/DoubanBoard/assets/DoubanBoard.0.1.js?v='.DoubanBoard_Plugin_VERSION.'"></script>';
     }
 }
 ?>
